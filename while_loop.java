f1b7c98ddb648cb2a5d9d80a07e567be05533193
itagarf <itagarfavour@gmail.com>
2023-03-20 11:19:56 +0000

    do-while loop

--- while_loop.java
+++ while_loop.java
@@ -19,5 +19,24 @@ class while_loop {
             }
             i++;
         }
+
+        //Do-while loop
+
+        int b = 5;
+        while (b <= 4){
+            System.out.println("Print this one!!!");
+            b++;
+        }  //This doesnt print anything because x is not less than or equal to 4
+
+        //Try this
+        int x = 5;
+        do{
+            System.out.println("Print this one!!!");
+            x++;
+        }while (x <= 4); //A semi-colon is required here because if you put the while before the curly brackets, the curly brackets becomes a statement, but if you put the while after the curly brackets, you need to end it with a semi-colon
+
+        //The previous statement did not print anything because x is not less than or equal to 4 but the next one will execute the statement at least once before checking the condition
+
+
     }
 }
